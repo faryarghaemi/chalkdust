389d34ec51dc621c4bdc949c0b76799c386f28e7
Faryar Ghaemi <faryarg88@gmail.com>
2015-04-14 15:57:15 +1000

    hello jack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Chalkdust::Application.configure do
 
   # Mailer
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV["SMTP_SERVER"] }
+  config.action_mailer.default_url_options = { :host => "chalkdust.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_SERVER"],
